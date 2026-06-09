2ec7ae35cb309657defb765abba3a9e6dc8cae77
CikguHisyam91 <g-69407950@moe-dl.edu.my>
2026-06-09 21:55:05 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE='w700-v2';
+const FILES=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES).catch(()=>{})));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(
+    ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+  )));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+      const clone=res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,clone));
+      return res;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
