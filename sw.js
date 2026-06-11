09d6c9390d7d732327ea93b7335fb1b811799390
CikguHisyam91 <g-69407950@moe-dl.edu.my>
2026-06-11 08:30:07 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'w700-v7';
+const CACHE = 'w700-v8';
 const STATIC = [
   './', './index.html', './manifest.json', './icon.svg',
   'https://fonts.googleapis.com/css2?family=Amiri:wght@400;700&display=swap',
